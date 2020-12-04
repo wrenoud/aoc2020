fc666379f6e84d2acef78074b9be02c5aaa66ec8
Weston Renoud <wrenoud@gmail.com>
2020-12-04 22:07:09 +0100

    format

--- template.py
+++ template.py
@@ -2,14 +2,16 @@ import util
 
 
 def part1(data):
-	util.Answer(1, None)
+    util.Answer(1, None)
 
-		
+
 def part2(data):
-	util.Answer(2, None)
+    util.Answer(2, None)
 
 
 if __name__ == "__main__":
-	data = util.ReadPuzzle()
-	part1(data)
-	part2(data)
+    data = util.ReadPuzzle()
+    for v in data:
+        print(v)
+    part1(data)
+    part2(data)
--- util.py
+++ util.py
@@ -6,63 +6,64 @@ import requests
 
 
 def get_session():
-	"""loads a adventofcode.com session cookie from env.json"""
-	env = json.load(open('env.json'))
-	cookie = requests.cookies.create_cookie(name='session', value=env['session'])
-	sess = requests.session()
-	sess.cookies.set_cookie(cookie)
-	return sess
+    """loads a adventofcode.com session cookie from env.json"""
+    env = json.load(open("env.json"))
+    cookie = requests.cookies.create_cookie(name="session", value=env["session"])
+    sess = requests.session()
+    sess.cookies.set_cookie(cookie)
+    return sess
 
 
 def Download(filepattern, urlpattern, day):
-	filepath = filepattern.format(day)
-	url = urlpattern.format(day)
+    filepath = filepattern.format(day)
+    url = urlpattern.format(day)
 
-	if not os.path.exists(filepath):
-		print("Downloading from adventofcode.com")
-		print(url)
-		res = get_session().get(url)
-		if res.status_code != 200:
-			print("*** error downloading ***")
-			return None
-		puzzle = res.text.strip()
-		with open(filepath, 'w') as f:
-			f.write(puzzle)
-		return puzzle.split('\n')
-	else:
-		print(url)
+    if not os.path.exists(filepath):
+        print("Downloading from adventofcode.com")
+        print(url)
+        res = get_session().get(url)
+        if res.status_code != 200:
+            print("*** error downloading ***")
+            return None
+        puzzle = res.text.strip()
+        with open(filepath, "w") as f:
+            f.write(puzzle)
+        return puzzle.split("\n")
+    else:
+        print(url)
 
-	with open(filepath) as f:
-		return f.read().split('\n')
+    with open(filepath) as f:
+        return f.read().split("\n")
 
 
 def ReadData(day: int) -> List[str]:
-	return Download("./data/puzzle_{:02}.txt",
-					"https://adventofcode.com/2019/day/{}/input", day)
+    return Download(
+        "./data/puzzle_{:02}.txt", "https://adventofcode.com/2020/day/{}/input", day
+    )
 
 
 def ReadInstructions(day: int):
-	Download("./data/instructions_{:02}.html",
-			"https://adventofcode.com/2019/day/{}", day)
+    Download(
+        "./data/instructions_{:02}.html", "https://adventofcode.com/2020/day/{}", day
+    )
 
 
 def ReadPuzzle():
-	day = int(sys.argv[0].split('_')[-1].split('.')[0])
-	ReadInstructions(day)
-	return ReadData(day)
+    day = int(sys.argv[0].split("_")[-1].split(".")[0])
+    ReadInstructions(day)
+    return ReadData(day)
 
 
 def Answer(part, answer):
-	print("-"*80)
-	print("Part {} answer:".format(part), answer)
-	print("-"*80)
+    print("-" * 80)
+    print("Part {} answer:".format(part), answer)
+    print("-" * 80)
 
 
 class AdventOfCodeDay:
-	pass
+    pass
 
 
 if __name__ == "__main__":
 
-	print(ReadData(1))
-
+    print(ReadData(1))
